eea02e862248666de25ee5ca3c1947ddeece877e
“Richard Middleton” <Richard@richardmiddleton.me>
2017-05-13 01:03:28 +0200

    changes to stroke background

--- main.js
+++ main.js
@@ -2,20 +2,22 @@ var mainCanvas = document.getElementById("myCanvas");
 var mainContext = mainCanvas.getContext("2d");
  
 function draw() {
+
+    mainContext.beginPath();
+    mainContext.arc(150, 150, 140, 0*Math.PI, 2 * Math.PI, false);
+ 
+    // draw the stroke
+    mainContext.lineWidth = 15;
+    mainContext.strokeStyle = "rgba(36, 38, 54, 1.0)";
+    mainContext.stroke();
+
+
     mainContext.beginPath();
     mainContext.arc(150, 150, 140, 1.5*Math.PI, 1 * Math.PI, false);
  
     // draw the stroke
     mainContext.lineWidth = 15;
     mainContext.strokeStyle = "rgba(216, 0, 96, 1.0)";
-    mainContext.stroke();
-
-    mainContext.beginPath();
-    mainContext.arc(150, 150, 140, 1*Math.PI, 1.5 * Math.PI, false);
- 
-    // draw the stroke
-    mainContext.lineWidth = 15;
-    mainContext.strokeStyle = "rgba(36, 38, 54, 1.0)";
     mainContext.stroke();
 }
     draw();
