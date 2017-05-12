11a640a1fe12c090cdc297b56d3be1dc8fdba7a0
“Richard Middleton” <Richard@richardmiddleton.me>
2017-05-12 23:53:39 +0200

    centered timer and canvas

--- main.js
+++ main.js
@@ -3,11 +3,19 @@ var mainContext = mainCanvas.getContext("2d");
  
 function draw() {
     mainContext.beginPath();
-    mainContext.arc(200, 460, 160, 1.5*Math.PI, 1 * Math.PI, false);
+    mainContext.arc(150, 150, 140, 1.5*Math.PI, 1 * Math.PI, false);
  
     // draw the stroke
     mainContext.lineWidth = 15;
-    mainContext.strokeStyle = "rgba(197, 35, 112, 1.0)";
+    mainContext.strokeStyle = "rgba(216, 0, 96, 1.0)";
+    mainContext.stroke();
+
+    mainContext.beginPath();
+    mainContext.arc(150, 150, 140, 1*Math.PI, 1.5 * Math.PI, false);
+ 
+    // draw the stroke
+    mainContext.lineWidth = 15;
+    mainContext.strokeStyle = "rgba(36, 38, 54, 1.0)";
     mainContext.stroke();
 }
     draw();
