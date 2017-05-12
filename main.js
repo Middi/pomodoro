6f338c9c240247c8f04f47a2d1294d4476496097
“Richard Middleton” <Richard@richardmiddleton.me>
2017-05-13 01:31:32 +0200

    width of canvas

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function draw() {
     mainContext.arc(150, 150, 100, 0*Math.PI, 2 * Math.PI, false);
  
     // draw the stroke
-    mainContext.lineWidth = 15;
+    mainContext.lineWidth = 10;
     mainContext.strokeStyle = "rgba(36, 38, 54, 1.0)";
     mainContext.stroke();
 
@@ -16,7 +16,7 @@ function draw() {
     mainContext.arc(150, 150, 100, 1.5*Math.PI, 1 * Math.PI, false);
  
     // draw the stroke
-    mainContext.lineWidth = 15;
+    mainContext.lineWidth = 10;
     mainContext.strokeStyle = "rgba(216, 0, 96, 1.0)";
     mainContext.stroke();
 }
